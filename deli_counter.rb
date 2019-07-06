175775a72ed37c8259b53c64676fa017b1bbc7c5
Viraj Ransing <rajhoustontexas@gmail.com>
2019-07-06 22:16:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,11 @@
 katz_deli = []
-def line 
+def line (katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each.with_index(1) do |name, index|
+      katz_deli.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{katz_deli.join(" ")}"
+  end
+end
