94610cd21723b234e87da26908cad74ef5e39eda
Viraj Ransing <rajhoustontexas@gmail.com>
2019-07-06 22:24:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,7 @@ def line (katz_deli)
     puts "The line is currently empty."
   else
     message="The line is currently:"
+    
     katz_deli.each_with_index do |name, index|
       message += " #{index.to_i+1}. #{name}"
     end
